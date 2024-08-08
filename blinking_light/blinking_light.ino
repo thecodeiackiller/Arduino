8a9f3d8201199ef4ec0a266214bcf2e24d17fd34
knunn553 <knunn553@outlook.com>
2024-08-08 19:34:43 -0400

    Removed the repeated code abiding by the DRY do not repeat yourself principle. This code currently has two blue and red leds replicated like a police car. There is a green LED light, too, which can be called again if we move the range for the for loops up by 1. Other than that, this is pretty much it for the blinking LED's.

--- blinking_light/blinking_light.ino
+++ blinking_light/blinking_light.ino
@@ -1,105 +1,29 @@
+int redBlink = 5;
+int blueBlink = 10;
+int greenBlink = 15;
+
+int pinNumber[] = {2,3,4};
+int blinkCount[] = {redBlink, blueBlink, greenBlink};
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
+  for(int i = 0; i < 2; i++)
+  {
+    pinMode(pinNumber[i], OUTPUT);
+  }
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(2,HIGH);
-  delay(1000);
-  digitalWrite(2,LOW);
-  delay(1000);
-digitalWrite(2,HIGH);
-  delay(1000);
-  digitalWrite(2,LOW);
-  delay(1000);
-  digitalWrite(2,HIGH);
-  delay(1000);
-  digitalWrite(2,LOW);
-  delay(1000);
-  digitalWrite(2,HIGH);
-  delay(1000);
-  digitalWrite(2,LOW);
-  delay(1000);
-  digitalWrite(2,HIGH);
-  delay(1000);
-  digitalWrite(2,LOW);
-  delay(1000);
-
-  digitalWrite(3,HIGH);
-  delay(1000);
-  digitalWrite(3,LOW);
-  delay(1000);
-digitalWrite(3,HIGH);
-  delay(1000);
-  digitalWrite(3,LOW);
-  delay(1000);
-  digitalWrite(3,HIGH);
-  delay(1000);
-  digitalWrite(3,LOW);
-  delay(1000);
-  digitalWrite(3,HIGH);
-  delay(1000);
-  digitalWrite(3,LOW);
-  delay(1000);
-  digitalWrite(3,HIGH);
-  delay(1000);
-  digitalWrite(3,LOW);
-  delay(1000);
-  digitalWrite(3,HIGH);
-  delay(1000);
-  digitalWrite(3,LOW);
-  delay(1000);
-  digitalWrite(3,HIGH);
-  delay(1000);
-  digitalWrite(3,LOW);
-  delay(1000);
-  digitalWrite(3,HIGH);
-  delay(1000);
-  digitalWrite(3,LOW);
-  delay(1000);
-
-  digitalWrite(4,HIGH);
-  delay(1000);
-  digitalWrite(4,LOW);
-  delay(1000);
-  digitalWrite(4,HIGH);
-  delay(1000);
-  digitalWrite(4,LOW);
-  delay(1000);
-  digitalWrite(4,HIGH);
-  delay(1000);
-  digitalWrite(4,LOW);
-  delay(1000);
-  digitalWrite(4,HIGH);
-  delay(1000);
-  digitalWrite(4,LOW);
-  delay(1000);
-  digitalWrite(4,HIGH);
-  delay(1000);
-  digitalWrite(4,LOW);
-  delay(1000);
-  digitalWrite(4,HIGH);
-  delay(1000);
-  digitalWrite(4,LOW);
-  delay(1000);
-  digitalWrite(4,HIGH);
-  delay(1000);
-  digitalWrite(4,LOW);
-  delay(1000);
-  digitalWrite(4,HIGH);
-  delay(1000);
-  digitalWrite(4,LOW);
-  delay(1000);
-  digitalWrite(4,HIGH);
-  delay(1000);
-  digitalWrite(4,LOW);
-  delay(1000);
-  digitalWrite(4,HIGH);
-  delay(1000);
-  digitalWrite(4,LOW);
-  delay(1000);
-  
+ for(int i = 0; i < 2; i++)
+ {
+    for(int j = 0; j < blinkCount[i]; j++)
+    {
+      digitalWrite(pinNumber[i],HIGH);
+      delay(50);
+      digitalWrite(pinNumber[i],LOW);
+      delay(50);
+    }
+ }
 }
