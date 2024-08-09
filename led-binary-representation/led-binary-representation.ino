bf9f11b5444cfa805b59248f9f4c7226088f550d
knunn553 <knunn553@outlook.com>
2024-08-09 14:08:15 -0400

    Started working on binary 5 bit representation of numbers 0-31. Program did not compile successfully but think this was due to hardware problem with faulty wire connection of LEDs on breadboard. Need to continue fixing circuit. Overall, learned a ton today including furthering my knowledge on how things are working under the hood, how each switch represents an on or off and how physics including electrons play a role in making everything happen. Also went over things like memory, memory addresses, pointers, and how they relate to what were doing. Also got to see how much memory is taken up and what actually happens electrically after we declare and/or initialize a variable.

---
+++ led-binary-representation/led-binary-representation.ino
@@ -0,0 +1,19 @@
+int light1 = 2;
+int light2 = 3;
+int light3 = 4;
+int light4 = 5;
+int light5 = 6;
+
+void setup() {
+  // pinMode(light1, OUTPUT);
+  // pinMode(light2, OUTPUT);
+  // pinMode(light3, OUTPUT);
+  // pinMode(light4, OUTPUT);
+  pinMode(light1, OUTPUT);
+ // This needs to be in a loop, of course
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(light1, HIGH);
+}
