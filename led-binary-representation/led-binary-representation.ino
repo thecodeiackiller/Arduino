00fd70ce2db23ec493493a42d6549814c41e5340
knunn553 <knunn553@outlook.com>
2024-08-12 16:16:37 -0400

    Was going to create an unsigned 5 bit representation of numbers of the way up to 31 but don't know the algorithm to do that yet and don't want to type all of that out so I just created a quick little relay thing that goes back and forth.

--- led-binary-representation/led-binary-representation.ino
+++ led-binary-representation/led-binary-representation.ino
@@ -3,17 +3,75 @@ int light2 = 3;
 int light3 = 4;
 int light4 = 5;
 int light5 = 6;
+int dly = 50;
 
 void setup() {
-  // pinMode(light1, OUTPUT);
-  // pinMode(light2, OUTPUT);
-  // pinMode(light3, OUTPUT);
-  // pinMode(light4, OUTPUT);
   pinMode(light1, OUTPUT);
+  pinMode(light2, OUTPUT);
+  pinMode(light3, OUTPUT);
+  pinMode(light4, OUTPUT);
+  pinMode(light5, OUTPUT);
  // This needs to be in a loop, of course
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  digitalWrite(light1, LOW);
+  digitalWrite(light2, LOW);
+  digitalWrite(light3, LOW);
+  digitalWrite(light4, LOW);
+  digitalWrite(light5, HIGH);
+  delay(dly);
+
+  digitalWrite(light1, LOW);
+  digitalWrite(light2, LOW);
+  digitalWrite(light3, LOW);
+  digitalWrite(light4, HIGH);
+  digitalWrite(light5, LOW);
+  delay(dly);
+
+  digitalWrite(light1, LOW);
+  digitalWrite(light2, LOW);
+  digitalWrite(light3, HIGH);
+  digitalWrite(light4, LOW);
+  digitalWrite(light5, LOW);
+  delay(dly);
+  
+  digitalWrite(light1, LOW);
+  digitalWrite(light2, HIGH);
+  digitalWrite(light3, LOW);
+  digitalWrite(light4, LOW);
+  digitalWrite(light5, LOW);
+  delay(dly);
+
   digitalWrite(light1, HIGH);
+  digitalWrite(light2, LOW);
+  digitalWrite(light3, LOW);
+  digitalWrite(light4, LOW);
+  digitalWrite(light5, LOW);
+  delay(dly);
+
+  digitalWrite(light1, LOW);
+  digitalWrite(light2, HIGH);
+  digitalWrite(light3, LOW);
+  digitalWrite(light4, LOW);
+  digitalWrite(light5, LOW);
+  delay(dly);
+
+  digitalWrite(light1, LOW);
+  digitalWrite(light2, LOW);
+  digitalWrite(light3, HIGH);
+  digitalWrite(light4, LOW);
+  digitalWrite(light5, LOW);
+  delay(dly);
+
+  digitalWrite(light1, LOW);
+  digitalWrite(light2, LOW);
+  digitalWrite(light3, LOW);
+  digitalWrite(light4, HIGH);
+  digitalWrite(light5, LOW);
+  delay(dly);
 }
+
+  
